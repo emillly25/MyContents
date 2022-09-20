30ce8f3c369c223d8c7cab54dabe1429921ffe48
sehee <seeeh0128@gmail.com>
2022-09-21 00:29:20 +0900

    배포 오류

--- server/src/app.js
+++ server/src/app.js
@@ -27,7 +27,7 @@ app.use("/login", loginRouter);
 app.use("/register", registerRouter);
 
 app.use(express.static(path.join(__dirname, "../build")));
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
